2a8198b1739607bbff0372167c8b3315bef26e4b
guams <guamsmdr@gmail.com>
2023-12-15 13:20:05 +0100

    Ajout exponentiation modulaire

--- algorithme_euclide.py
+++ algorithme_euclide.py
@@ -1,3 +1,5 @@
+import decomposition_nombre_premier
+
 def bezout(a, b):
     xa, ya, xb, yb = 1, 0, 0, 1
     while b != 0:
@@ -7,4 +9,32 @@ def bezout(a, b):
         return -a, -xa, -ya
     return a, xa, ya
 
-print(bezout(368, -117))
+# print(bezout(242, 51))
+# V = 19 => (51*19)%242 = 1 => 19 = d car inverse de e
+# (eV)%242 = 1 => (ed)%242 = 1
+# print((51*19)%242)
+
+def base10_to_bin(x):
+    x = list(bin(x))[2:]
+    for i in range(len(x)):
+        x[i] = int(x[i])
+    return x[::-1]
+
+def exponentiation_modulaire(x, p, Zn):
+    x = x % Zn
+    p = base10_to_bin(p)
+    result = []
+    result_f = 1
+    j = 1
+    k = 0
+    for i in range(len(p)):
+        if p[i] == 1:
+            temp = (x**j)%Zn
+            result.append(temp)
+            result_f*= result[k]
+            k+=1
+        j= j*2
+    return result_f%Zn
+
+
+
---
+++ rsa.py
@@ -0,0 +1,4 @@
+# Je toruche le RSA
+import decomposition_nombre_premier
+import algorithme_euclide
+
